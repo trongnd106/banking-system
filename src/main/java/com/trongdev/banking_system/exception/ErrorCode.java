2ee6385587eca878d03334a8b81f915aecfeba52
trongnd106 <trong.nd106@gmail.com>
2024-09-18 11:27:48 +0700

    Update: error code

--- src/main/java/com/trongdev/banking_system/exception/ErrorCode.java
+++ src/main/java/com/trongdev/banking_system/exception/ErrorCode.java
@@ -10,9 +10,10 @@ public enum ErrorCode {
     USERNAME_INVALID(1001, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
     PASSWORD_INVALID(1002, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
     EMAIL_INVALID(1003, "Email must include character '@'", HttpStatus.BAD_REQUEST),
-    USER_NOT_EXISTED(1004, "Not found user", HttpStatus.NOT_FOUND),
-    UNAUTHENTICATED(2001, "Unauthenticated", HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZED(3001, "Do not have permission", HttpStatus.FORBIDDEN),
+    DOB_INVALID(1004, "Your age must be at least 18", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(2001, "Not found user", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(3001, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(3002, "Do not have permission", HttpStatus.FORBIDDEN),
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
     ;
     private int code;
